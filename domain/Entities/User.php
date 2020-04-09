9fe336d60f3eda1fdef4ac700342fba47d6a4900
cristianvena18 <cristiandamianvena@gmail.com>
2020-04-09 15:49:50 -0300

    added all authentication and mailing

--- domain/Entities/User.php
+++ domain/Entities/User.php
@@ -1,7 +1,134 @@
 <?php
 
+declare(strict_types=1);
+
+namespace Domain\Entities;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Exception;
+use function Sodium\add;
 
 class User
 {
+    private int $id;
+    private string $name;
+    private string $surname;
+    private string $username;
+    private string $email;
+    private string $password;
+    private bool $isActive;
+    private $roles;
 
+    /**
+     * Activity constructor.
+     *
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        $this->isActive = true;
+    }
+
+    /**
+     * @return string $name
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string $name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @return string $email
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string $password
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param bool $isActive
+     */
+    public function setIsActive(bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRole($role): void
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/send', function () {
+    Mail::send('email', [], function($message) {
+        $message->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+        $message->to('joaquinmartina44@gmail.com', 'joaquin martina')->subject('hola');
+    });
+    Mail::send('email', [], function($message) {
+        $message->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+        $message->to('martinmontanari3@gmail.com', 'Martin montanari')->subject('hola');
+    });
+    Mail::send('email', [], function($message) {
+        $message->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+        $message->to('ayrtoncravero26@gmail.com', 'Ayrton cravero')->subject('hola');
+    });
+    return 'correo enviado!';
+});
