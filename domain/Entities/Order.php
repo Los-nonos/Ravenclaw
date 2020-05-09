a8104e6929b2c063d7a06606e27a8dd01158866b
cristianvena18 <cristiandamianvena@gmail.com>
2020-05-08 22:11:16 -0300

    added parameters in order

--- domain/Entities/Order.php
+++ domain/Entities/Order.php
@@ -4,13 +4,78 @@
 namespace Domain\Entities;
 
 
+use DateTime;
+
 class Order
 {
-    private $id;
+    private int $id;
 
-    private $amount;
+    private float $amount;
 
-    private $date;
+    private DateTime $date;
 
     private bool $charged;
+
+    public function __construct(float $amount, DateTime $date, bool $charged)
+    {
+        $this->amount = $amount;
+        $this->charged = $charged;
+        $this->date = $date;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param float $amount
+     */
+    public function setAmount(float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param DateTime $date
+     */
+    public function setDate(DateTime $date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCharged(): bool
+    {
+        return $this->charged;
+    }
+
+    /**
+     * @param bool $charged
+     */
+    public function setCharged(bool $charged): void
+    {
+        $this->charged = $charged;
+    }
 }
